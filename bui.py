e873f0c23bb19fd24a576bfd023734b946af28db
nitish145-os <77478808+nitish145-os@users.noreply.github.com>
2021-12-22 20:46:41 +0530

    Update bui.py

--- bui.py
+++ bui.py
@@ -7,11 +7,15 @@ root = tk.Tk()
 EventButton = tk.Button(root, text="Ëvent")
 EventButton.grid(column=1, row=1)
 
+
 ChatButton = tk.Button(root, text="Chat")
 ChatButton.grid(column=2, row=1)
 
 ClassButton = tk.Button(root, text="Calls")
 ClassButton.grid(column=3, row=1)
+
+PlayButton = tk.Button(root, text="Play")
+PlayButton.grid(column=4, row=1)
 
 
 root.mainloop()
